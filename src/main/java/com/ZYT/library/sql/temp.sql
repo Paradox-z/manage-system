f43205e31bb9ffa182c3df5c18eaad79dfd5a56e
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2023-12-10 12:46:39 +0800

    Update temp.sql
    essence.

--- src/main/java/com/ZYT/library/sql/temp.sql
+++ src/main/java/com/ZYT/library/sql/temp.sql
@@ -3,9 +3,9 @@ VALUES (?, ?, ?, ?, NOW(), 1, ?, ?);
 
 select * from sys_user where state = '1';
 INSERT INTO sys_user (`uid`, `username`, `password`, `realname`, `createtime`, `state`, `mobile`, `amount`)
-VALUES ('1001', 'johndoe', 'mypassword', 'John Doe', '2023-05-13 20:07:15', 1, '1234567890', 5000.00);
+VALUES ('1001', 'jensen', 'password', 'Jensen', '2023-12-10 12:45:15', 1, '12345678901', 5000.00);
 
-select * from sys_user where uid = 22132048 and state = 1
+select * from sys_user where uid = 1001 and state = 1
 
 select * from books_info where state = '2';
 
