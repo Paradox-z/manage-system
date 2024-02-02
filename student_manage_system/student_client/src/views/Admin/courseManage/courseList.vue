f2b34973a3bf6fb15c5a2cc4295048965d1b334c
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-02-03 00:42:59 +0800

    Update courseList.vue

--- student_manage_system/student_client/src/views/Admin/courseManage/courseList.vue
+++ student_manage_system/student_client/src/views/Admin/courseManage/courseList.vue
@@ -30,7 +30,7 @@
               cancel-button-text='Cancel'
               icon="el-icon-info"
               icon-color="red"
-              title="删除不可复原"
+              title="Deleted information that cannot be recovered."
               @confirm="deleteTeacher(scope.row)"
           >
             <el-button slot="reference" type="text" size="small">删除</el-button>
