8e59be75055443b976cf3538366ddf6a745bbbfb
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-03-05 01:08:13 +0800

    Update editorGradeCourse.vue

--- student_manage_system/student_client/src/views/Admin/gradeCourseManage/editorGradeCourse.vue
+++ student_manage_system/student_client/src/views/Admin/gradeCourseManage/editorGradeCourse.vue
@@ -88,7 +88,7 @@ export default {
             if (resp.data === true) {
               that.$message({
                 showClose: true,
-                message: '编辑成功',
+                message: 'Edit successfully.',
                 type: 'success'
               });
             }
