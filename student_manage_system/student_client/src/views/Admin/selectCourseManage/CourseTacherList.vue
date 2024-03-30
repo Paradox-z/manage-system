877738d63444839b644a0004dd0f97a0618a8665
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-03-31 00:12:34 +0800

    Update CourseTacherList.vue

--- student_manage_system/student_client/src/views/Admin/selectCourseManage/CourseTacherList.vue
+++ student_manage_system/student_client/src/views/Admin/selectCourseManage/CourseTacherList.vue
@@ -28,7 +28,7 @@
           width="150">
       </el-table-column>
       <el-table-column
-          label="操作"
+          label="Operation"
           width="100">
         <template slot-scope="scope">
           <el-popconfirm
