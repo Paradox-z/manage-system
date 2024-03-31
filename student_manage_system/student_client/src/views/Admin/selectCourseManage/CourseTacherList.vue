67c4f791e7fdc93de13c241272733434b782564b
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-04-01 01:59:35 +0800

    Update CourseTacherList.vue

--- student_manage_system/student_client/src/views/Admin/selectCourseManage/CourseTacherList.vue
+++ student_manage_system/student_client/src/views/Admin/selectCourseManage/CourseTacherList.vue
@@ -32,7 +32,7 @@
           width="100">
         <template slot-scope="scope">
           <el-popconfirm
-              confirm-button-text='删除'
+              confirm-button-text='Delete'
               cancel-button-text='取消'
               icon="el-icon-info"
               icon-color="red"
