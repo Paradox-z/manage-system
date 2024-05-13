abda7138bdea78e35d24adc7b67d684acdcf2bb8
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-05-14 00:13:19 +0800

    Update addStudent.vue

--- student_manage_system/student_client/src/views/Admin/studentManage/addStudent.vue
+++ student_manage_system/student_client/src/views/Admin/studentManage/addStudent.vue
@@ -25,7 +25,7 @@ export default {
       },
       rules: {
         sname: [
-          { required: true, message: '请输入名称', trigger: 'blur' },
+          { required: true, message: 'Please enter the name:', trigger: 'blur' },
           { min: 2, max: 5, message: '长度在 2 到 5 个字符', trigger: 'blur' }
         ],
         password: [
