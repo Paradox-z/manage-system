db0f080c7f9c64aa838413490c56122c075c6bc5
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-06-04 00:25:25 +0800

    Update queryStudent.vue

--- student_manage_system/student_client/src/views/Admin/studentManage/queryStudent.vue
+++ student_manage_system/student_client/src/views/Admin/studentManage/queryStudent.vue
@@ -14,7 +14,7 @@
               <el-switch v-model="ruleForm.password"></el-switch>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">查询</el-button>
+              <el-button type="primary" @click="submitForm('ruleForm')">Inquiring</el-button>
               <el-button @click="resetForm('ruleForm')">重置</el-button>
               <el-button @click="flush('ruleForm')">刷新</el-button>
             </el-form-item>
