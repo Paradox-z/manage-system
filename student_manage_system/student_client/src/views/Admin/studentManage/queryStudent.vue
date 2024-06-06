2b4438ab7d969f28e567e6de7c123c8cb8b28d57
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-06-07 00:30:40 +0800

    Update queryStudent.vue

--- student_manage_system/student_client/src/views/Admin/studentManage/queryStudent.vue
+++ student_manage_system/student_client/src/views/Admin/studentManage/queryStudent.vue
@@ -16,7 +16,7 @@
             <el-form-item>
               <el-button type="primary" @click="submitForm('ruleForm')">Inquiring</el-button>
               <el-button @click="resetForm('ruleForm')">Reset</el-button>
-              <el-button @click="flush('ruleForm')">刷新</el-button>
+              <el-button @click="flush('ruleForm')">Flush</el-button>
             </el-form-item>
           </el-form>
         </el-card>
