8f0074f3bfdda484075616fd904d313184236546
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-06-14 08:17:00 +0800

    Update studentList.vue

--- student_manage_system/student_client/src/views/Admin/studentManage/studentList.vue
+++ student_manage_system/student_client/src/views/Admin/studentManage/studentList.vue
@@ -21,7 +21,7 @@
           width="120">
       </el-table-column>
       <el-table-column
-          label="操作"
+          label="Operate"
           width="100">
         <template slot-scope="scope">
           <el-popconfirm
