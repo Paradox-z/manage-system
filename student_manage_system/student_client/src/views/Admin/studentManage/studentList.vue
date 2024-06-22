4f90d63ec3d6f1fa9a512969d197618014d00b7a
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2024-06-22 16:47:31 +0700

    Update studentList.vue

--- student_manage_system/student_client/src/views/Admin/studentManage/studentList.vue
+++ student_manage_system/student_client/src/views/Admin/studentManage/studentList.vue
@@ -72,7 +72,7 @@ export default {
         else {
           that.$message({
             showClose: true,
-            message: '删除出错，请查询数据库连接',
+            message: 'Failed to delete, please checking your connection to database.',
             type: 'error'
           });
         }
